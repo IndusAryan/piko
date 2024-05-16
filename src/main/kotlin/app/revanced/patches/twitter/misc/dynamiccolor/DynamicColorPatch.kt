1a2740edc6b78f2bf3acd61a4a7aa1f0a3539486
IndusAryan <serena01011975@gmail.com>
2024-05-16 14:03:44 +0530

    💀

--- src/main/kotlin/app/revanced/patches/twitter/misc/dynamiccolor/DynamicColorPatch.kt
+++ src/main/kotlin/app/revanced/patches/twitter/misc/dynamiccolor/DynamicColorPatch.kt
@@ -10,7 +10,7 @@ import java.nio.file.Files
 
 @Patch(
     name = "Dynamic color",
-    description = "Replaces the default Blue accent with the user's Material You palette and Dark Dim, Light Theme with Full Material Design",
+    description = "Replaces the default Blue accent with the user's Material You palette and Dim, Light Theme with Full Material Design",
     compatiblePackages = [CompatiblePackage("com.twitter.android")],
 )
 @Suppress("unused")
@@ -27,128 +27,134 @@ object DynamicColorPatch : ResourcePatch() {
 
         listOf(valuesV31Directory, valuesNightV31Directory).forEach { it ->
             val colorsXml = it.resolve("colors.xml")
+            val stylesXml = it.resolve("styles.xml")
 
             if (!colorsXml.exists()) {
                 FileWriter(colorsXml).use {
                     it.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><resources></resources>")
                 }
             }
-        }
-
-        context.xmlEditor["res/values-v31/colors.xml"].use { editor ->
-            val document = editor.file
-
-            mapOf(
-                "ps__twitter_blue" to "@color/twitter_blue",
-                "ps__twitter_blue_pressed" to "@color/twitter_blue_fill_pressed",
-                "twitter_blue" to "@android:color/system_accent1_400",
-                "twitter_blue_fill_pressed" to "@android:color/system_accent1_300",
-                "twitter_blue_opacity_30" to "@android:color/system_accent1_100",
-                "twitter_blue_opacity_50" to "@android:color/system_accent1_200",
-                "twitter_blue_opacity_58" to "@android:color/system_accent1_300",
-                "deep_transparent_twitter_blue" to "@android:color/system_accent1_200",
-            ).forEach { (k, v) ->
-                val colorElement = document.createElement("color")
-
-                colorElement.setAttribute("name", k)
-                colorElement.textContent = v
-
-                document.getElementsByTagName("resources").item(0).appendChild(colorElement)
-            }
-        }
-
-        context.xmlEditor["res/values-night-v31/colors.xml"].use { editor ->
-            val document = editor.file
-
-            mapOf(
-                "twitter_blue" to "@android:color/system_accent1_200",
-                "twitter_blue_fill_pressed" to "@android:color/system_accent1_300",
-                "twitter_blue_opacity_30" to "@android:color/system_accent1_50",
-                "twitter_blue_opacity_50" to "@android:color/system_accent1_100",
-                "twitter_blue_opacity_58" to "@android:color/system_accent1_200",
-                "deep_transparent_twitter_blue" to "@android:color/system_accent1_200",
-            ).forEach { (k, v) ->
-                val colorElement = document.createElement("color")
-
-                colorElement.setAttribute("name", k)
-                colorElement.textContent = v
-
-                document.getElementsByTagName("resources").item(0).appendChild(colorElement)
-            }
-        }
-
-        /** fun fullMaterialDesign() { **/
-        // backward compatible, creates style into v31 res dir (A12+)
-        // replace parts of DimTheme with user's material 3 neutral palette
-        context.xmlEditor["res/values-night-v31/styles.xml"].use { editor ->
-            val document = editor.file
-
-            val newStyle = document.createElement("style")
-            newStyle.setAttribute("name", "PaletteDim")
-            newStyle.setAttribute("parent", "@style/HorizonColorPaletteDark")
-
-            val styleItems = mapOf(
-                "abstractColorCellBackground" to "@color/material_dynamic_neutral10",
-                "abstractColorCellBackgroundTranslucent" to "@color/material_dynamic_neutral10",
-                "abstractColorDeepGray" to "#ff8899a6",
-                "abstractColorDivider" to "#ff38444d",
-                "abstractColorFadedGray" to "@color/material_dynamic_neutral10",
-                "abstractColorFaintGray" to "@color/material_dynamic_neutral10",
-                "abstractColorHighlightBackground" to "@color/material_dynamic_neutral20",
-                "abstractColorLightGray" to "#ff3d5466",
-                "abstractColorLink" to "@color/twitter_blue",
-                "abstractColorMediumGray" to "#ff6b7d8c",
-                "abstractColorText" to "@color/white",
-                "abstractColorUnread" to "#ff163043",
-                "abstractElevatedBackground" to "#ff1c2c3c",
-                "abstractElevatedBackgroundShadow" to "#1a15202b"
-            )
-
-            styleItems.forEach { (k, v) ->
-                val styleElement = document.createElement("item")
-
-                styleElement.setAttribute("name", k)
-                styleElement.textContent = v
-                newStyle.appendChild(styleElement)
+            if (!stylesXml.exists()) {
+                FileWriter(stylesXml).use {
+                    it.write("<?xml version=\"1.0\" encoding=\"utf-8\"?><resources></resources>")
+                }
             }
 
-            document.getElementsByTagName("resources").item(0).appendChild(newStyle)
-        }
+            context.xmlEditor["res/values-v31/colors.xml"].use { editor ->
+                val document = editor.file
 
-        // monet theme in light mode
-        context.xmlEditor["res/values-v31/styles.xml"].use { editor ->
-            val document = editor.file
+                mapOf(
+                    "ps__twitter_blue" to "@color/twitter_blue",
+                    "ps__twitter_blue_pressed" to "@color/twitter_blue_fill_pressed",
+                    "twitter_blue" to "@android:color/system_accent1_400",
+                    "twitter_blue_fill_pressed" to "@android:color/system_accent1_300",
+                    "twitter_blue_opacity_30" to "@android:color/system_accent1_100",
+                    "twitter_blue_opacity_50" to "@android:color/system_accent1_200",
+                    "twitter_blue_opacity_58" to "@android:color/system_accent1_300",
+                    "deep_transparent_twitter_blue" to "@android:color/system_accent1_200",
+                ).forEach { (k, v) ->
+                    val colorElement = document.createElement("color")
 
-            val newStyle = document.createElement("style")
-            newStyle.setAttribute("name", "PaletteStandard")
-            newStyle.setAttribute("parent", "@style/HorizonColorPaletteLight")
+                    colorElement.setAttribute("name", k)
+                    colorElement.textContent = v
 
-            val styleItems = mapOf(
-                "abstractColorCellBackground" to "@color/material_dynamic_primary95",
-                "abstractColorCellBackgroundTranslucent" to "@color/material_dynamic_primary95",
-                "abstractColorDeepGray" to "@color/gray_700",
-                "abstractColorDivider" to "@color/material_dynamic_secondary90",
-                "abstractColorFadedGray" to "@color/material_dynamic_secondary95",
-                "abstractColorFaintGray" to "@color/material_dynamic_secondary95",
-                "abstractColorHighlightBackground" to "@color/material_dynamic_secondary95",
-                "abstractColorLightGray" to "@color/gray_200",
-                "abstractColorLink" to "@color/twitter_blue",
-                "abstractColorMediumGray" to "@color/gray_300",
-                "abstractColorText" to "@color/gray_1100",
-                "abstractColorUnread" to "@color/blue_0",
-                "abstractElevatedBackground" to "@color/white",
-                "abstractElevatedBackgroundShadow" to "@color/black_opacity_10"
-            )
-
-            styleItems.forEach { (k, v) ->
-                val styleElement = document.createElement("item")
-
-                styleElement.setAttribute("name", k)
-                styleElement.textContent = v
-                newStyle.appendChild(styleElement)
+                    document.getElementsByTagName("resources").item(0).appendChild(colorElement)
+                }
             }
 
-            document.getElementsByTagName("resources").item(0).appendChild(newStyle)
+            context.xmlEditor["res/values-night-v31/colors.xml"].use { editor ->
+                val document = editor.file
+
+                mapOf(
+                    "twitter_blue" to "@android:color/system_accent1_200",
+                    "twitter_blue_fill_pressed" to "@android:color/system_accent1_300",
+                    "twitter_blue_opacity_30" to "@android:color/system_accent1_50",
+                    "twitter_blue_opacity_50" to "@android:color/system_accent1_100",
+                    "twitter_blue_opacity_58" to "@android:color/system_accent1_200",
+                    "deep_transparent_twitter_blue" to "@android:color/system_accent1_200",
+                ).forEach { (k, v) ->
+                    val colorElement = document.createElement("color")
+
+                    colorElement.setAttribute("name", k)
+                    colorElement.textContent = v
+
+                    document.getElementsByTagName("resources").item(0).appendChild(colorElement)
+                }
+            }
+
+            /** fun fullMaterialDesign() { **/
+            // backward compatible, creates style into v31 res dir (A12+)
+            // replace parts of DimTheme with user's material 3 neutral palette
+            context.xmlEditor["res/values-night-v31/styles.xml"].use { editor ->
+                val document = editor.file
+
+                val newStyle = document.createElement("style")
+                newStyle.setAttribute("name", "PaletteDim")
+                newStyle.setAttribute("parent", "@style/HorizonColorPaletteDark")
+
+                val styleItems = mapOf(
+                    "abstractColorCellBackground" to "@color/material_dynamic_neutral10",
+                    "abstractColorCellBackgroundTranslucent" to "@color/material_dynamic_neutral10",
+                    "abstractColorDeepGray" to "#ff8899a6",
+                    "abstractColorDivider" to "#ff38444d",
+                    "abstractColorFadedGray" to "@color/material_dynamic_neutral10",
+                    "abstractColorFaintGray" to "@color/material_dynamic_neutral10",
+                    "abstractColorHighlightBackground" to "@color/material_dynamic_neutral20",
+                    "abstractColorLightGray" to "#ff3d5466",
+                    "abstractColorLink" to "@color/twitter_blue",
+                    "abstractColorMediumGray" to "#ff6b7d8c",
+                    "abstractColorText" to "@color/white",
+                    "abstractColorUnread" to "#ff163043",
+                    "abstractElevatedBackground" to "#ff1c2c3c",
+                    "abstractElevatedBackgroundShadow" to "#1a15202b"
+                )
+
+                styleItems.forEach { (k, v) ->
+                    val styleElement = document.createElement("item")
+
+                    styleElement.setAttribute("name", k)
+                    styleElement.textContent = v
+                    newStyle.appendChild(styleElement)
+                }
+
+                document.getElementsByTagName("resources").item(0).appendChild(newStyle)
+            }
+
+            // monet theme in light mode
+            context.xmlEditor["res/values-v31/styles.xml"].use { editor ->
+                val document = editor.file
+
+                val newStyle = document.createElement("style")
+                newStyle.setAttribute("name", "PaletteStandard")
+                newStyle.setAttribute("parent", "@style/HorizonColorPaletteLight")
+
+                val styleItems = mapOf(
+                    "abstractColorCellBackground" to "@color/material_dynamic_primary95",
+                    "abstractColorCellBackgroundTranslucent" to "@color/material_dynamic_primary95",
+                    "abstractColorDeepGray" to "@color/gray_700",
+                    "abstractColorDivider" to "@color/material_dynamic_secondary90",
+                    "abstractColorFadedGray" to "@color/material_dynamic_secondary95",
+                    "abstractColorFaintGray" to "@color/material_dynamic_secondary95",
+                    "abstractColorHighlightBackground" to "@color/material_dynamic_secondary95",
+                    "abstractColorLightGray" to "@color/gray_200",
+                    "abstractColorLink" to "@color/twitter_blue",
+                    "abstractColorMediumGray" to "@color/gray_300",
+                    "abstractColorText" to "@color/gray_1100",
+                    "abstractColorUnread" to "@color/blue_0",
+                    "abstractElevatedBackground" to "@color/white",
+                    "abstractElevatedBackgroundShadow" to "@color/black_opacity_10"
+                )
+
+                styleItems.forEach { (k, v) ->
+                    val styleElement = document.createElement("item")
+
+                    styleElement.setAttribute("name", k)
+                    styleElement.textContent = v
+                    newStyle.appendChild(styleElement)
+                }
+
+                document.getElementsByTagName("resources").item(0).appendChild(newStyle)
+            }
         }
     }
 }
